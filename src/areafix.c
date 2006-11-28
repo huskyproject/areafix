5327ce58e2014d1cb21f172ac3d7ed0bdb10f9c1
Stas Degteff <stas_degteff@users.sourceforge.net>
2006-11-28 09:07:21 +0000

    Check result of MsgWriteMsg() always

--- src/areafix.c
+++ src/areafix.c
@@ -2582,7 +2582,8 @@ void afix(hs_addr addr, char *cmd)
                         MsgKillMsg(netmail, i--);
                     } else {
                         xmsg.attr |= MSGREAD;
-                        MsgWriteMsg(SQmsg, 0, &xmsg, NULL, 0, 0, 0, NULL);
+                        if( 0!=MsgWriteMsg(SQmsg, 0, &xmsg, NULL, 0, 0, 0, NULL) )
+                           w_log(LL_ERR, "Could not write msg in netmailarea %s! Check the wholeness of messagebase, please.", af_config->netMailAreas[k].areaName);
                         MsgCloseMsg(SQmsg);
                     }
                     freeMsgBuffers(&msg);
